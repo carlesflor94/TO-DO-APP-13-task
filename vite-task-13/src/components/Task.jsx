dec8fd1ee22513fb44be928fd76fa4c8b03a2962
carlesflor94 <cf.ferrandis@gmail.com>
2026-02-01 17:36:58 +0100

    added editing fuctionality

--- vite-task-13/src/components/Task.jsx
+++ vite-task-13/src/components/Task.jsx
@@ -1,22 +1,47 @@
+import { useState } from 'react'
 import { format } from 'date-fns'
 
 const Task = ({ task, changeState, handleDelete, editTaskDescription }) => {
 
-    const stringDate = format(task.time, 'PPpp')
+    const stringDate = format(task.time, 'PPpp');
+
+    const [editing, setEditing] = useState(false);
+    const [editedTask, setEditedTask] = useState(task.description);
 
     function handleTaskClick() {
       changeState(task.id)
+    }
+
+    function handleEdit(e) {
+      setEditedTask(e.target.value)
+    }
+
+    function changeEdit(e) {
+      if (e.key === 'Enter') {
+        editTaskDescription(task.id, editedTask)
+        setEditing(false);
+      }
     }
 
     return (
         <li className={task.complete ? 'completed' : ''}>
             <div className="view">
               <input className="toggle" type="checkbox" checked={task.complete} onChange={handleTaskClick}/>
-              <label>
-                <span className="description">{task.description}</span>
-                <span className="created">created {stringDate}</span>
+              {!editing ? (
+                <label>
+                  <span className="description">{task.description}</span>
+                  <span className="created">created {stringDate}</span>
               </label>
-              <button className="icon icon-edit" onClick={() => editTaskDescription(task.id)}></button>
+              ) : (
+                <input className="edit" value={editedTask} autoFocus onChange={handleEdit} onKeyDown={changeEdit} />
+              )
+            }
+              
+              <button className="icon icon-edit" onClick={() => {
+                    setEditedTask(task.description)
+                    setEditing(true)
+                  }}>
+                </button>
               <button className="icon icon-destroy" onClick={() => handleDelete(task.id)}></button>
             </div>
           </li>
